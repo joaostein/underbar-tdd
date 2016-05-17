c0d3d7b0c54763a3eef08407b323441a7848ad6a
Joao Stein <joaosteing@gmail.com>
2016-05-16 22:21:01 -0300

    add first solution

--- test.js
+++ test.js
@@ -7,24 +7,61 @@ var expect = chai.expect;
 var _ = require('./underbar');
 
 
-describe('#identify()', function () {
+describe('#identity()', function () {
   it('return the same value', function () {
-    expect(_.identify(1)).to.equal(1);
-    expect(_.identify('1')).to.equal('1');
-    expect(_.identify(undefined)).to.equal(undefined);
-    expect(_.identify(true)).to.equal(true);
-    expect(_.identify(null)).to.equal(null);
+    expect(_.identity(1)).to.equal(1);
+    expect(_.identity('1')).to.equal('1');
+    expect(_.identity(undefined)).to.equal(undefined);
+    expect(_.identity(true)).to.equal(true);
+    expect(_.identity(null)).to.equal(null);
 
     var myArray = [1, 2, 3];
-    expect(_.identify(myArray)).to.equal(myArray);
-    expect(_.identify(myArray)).to.not.equal([1, 2, 3]);
+    expect(_.identity(myArray)).to.equal(myArray);
+    expect(_.identity(myArray)).to.not.equal([1, 2, 3]);
 
     var myObj = { key: 'value', key1: 'value1' };
-    expect(_.identify(myObj)).to.equal(myObj);
-    expect(_.identify(myObj)).to.not.equal({ key: 'value', key1: 'value1' });
+    expect(_.identity(myObj)).to.equal(myObj);
+    expect(_.identity(myObj)).to.not.equal({ key: 'value', key1: 'value1' });
   });
 
   it('should handle validate empty arguments', function () {
-    expect(function () { _.identify(); }).to.throw(/Invalid Argument/);
+    expect(function () { _.identity(); }).to.throw();
+  });
+});
+
+describe('#first()', function () {
+  var myArray;
+
+  beforeEach(function () {
+    myArray = [1, 2, 3];
+  });
+
+  it('should return the first element of array', function () {
+    expect(_.first(myArray)).to.equal(1);
+  });
+
+  it('should return the first `n` elements of array', function () {
+    expect(_.first(myArray, 2)).to.deep.equal([1, 2]);
+  });
+
+  it('should return the first element with `n` equals one', function () {
+    expect(_.first(myArray, 1)).to.equal(1);
+  });
+
+  it('should return the first element with `n` equals zero', function () {
+    expect(_.first(myArray, 0)).to.equal(1);
+  });
+
+  it('should return all elements when `n` greater than array length', function () {
+    expect(_.first(myArray, 100)).to.deep.equal([1, 2, 3]);
+  });
+
+  it('should only accept arrays as first parameters', function () {
+    expect(function () { _.first(false); }).to.throw();
+    expect(function () { _.first(undefined); }).to.throw();
+    expect(function () { _.first(null); }).to.throw();
+    expect(function () { _.first({}); }).to.throw();
+    expect(function () { _.first(1); }).to.throw();
+    expect(function () { _.first('string'); }).to.throw();
   });
 });
--- underbar.js
+++ underbar.js
@@ -1,9 +1,47 @@
 'use strict';
 
-exports.identify = function (value) {
+
+// _.identity(value)
+//
+// Returns the same value that is used as the argument. In math: `f(x) = x`
+//
+// This function looks useless, but is used throughout Underscore as a default iteratee.
+//
+// var stooge = {name: 'moe'};
+// stooge === _.identity(stooge);
+// => true
+
+exports.identity = function (value) {
   if (arguments.length === 0) {
     throw new Error('Invalid Argument');
   }
 
   return value;
+};
+
+// _.first(array, [n])
+//
+// Returns the first element of an array. Passing n will return the first n elements of the array.
+
+// _.first([5, 4, 3, 2, 1]);
+// => 5
+
+exports.first = function (array, length) {
+  if (!Array.isArray(array)) {
+    throw new Error('Invalid Parameter');
+  }
+
+  if (length > 1) {
+    var elements = [];
+
+    length = length > array.length ? array.length : length;
+
+    for (var i = 0; i < length; i++) {
+      elements.push(array.shift());
+    }
+
+    return elements;
+  }
+
+  return array.shift();
 };
